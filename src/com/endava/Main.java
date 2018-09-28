69ebcb175f0257964e819313fc488acbd0e34981
MilaBezaliuc <bezaliuc@gmail.com>
2018-09-28 09:19:24 +0300

    added decoration

--- src/com/endava/Main.java
+++ src/com/endava/Main.java
@@ -3,6 +3,10 @@ package com.endava;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("****************");
         System.out.println("This is test!");
+        System.out.println("****************");
+
     }
 }
